fad91addf7691ecb3a8a9768056fc69604e73245
Mikołaj Pich <m.pich@outlook.com>
2021-10-30 12:12:40 +0200

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ val logback_version: String by project
 
 plugins {
     application
-    kotlin("jvm") version "1.5.21"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.21"
+    kotlin("jvm") version "1.5.31"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.31"
 }
 
 group = "io.github.wulkanowy"
--- src/main/kotlin/io/github/wulkanowy/manager/server/MainModule.kt
+++ src/main/kotlin/io/github/wulkanowy/manager/server/MainModule.kt
@@ -6,18 +6,21 @@ import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
+import kotlinx.serialization.json.Json
 import org.koin.dsl.module
 
 val mainModule = module {
     single { UnstableService(get()) }
     single { GithubRepository(get()) }
     single {
+        Json {
+            ignoreUnknownKeys = true
+        }
+    }
+    single {
         HttpClient(CIO) {
             install(JsonFeature) {
-                serializer = KotlinxSerializer(kotlinx.serialization.json.Json {
-                    ignoreUnknownKeys = true
-
-                })
+                serializer = KotlinxSerializer(get())
             }
         }
     }
