3e470fa65a039b603bf5c6468f087f8baaa50bdc
Mikołaj Pich <m.pich@outlook.com>
2021-10-30 20:07:56 +0200

    Make api requests for prs simultaneous

--- src/main/kotlin/io/github/wulkanowy/manager/server/models/PullRequestBuild.kt
+++ src/main/kotlin/io/github/wulkanowy/manager/server/models/PullRequestBuild.kt
@@ -1,18 +1,39 @@
 package io.github.wulkanowy.manager.server.models
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class PullRequestBuild(
+    @SerialName("pull_reqest")
+    val pullRequest: PullRequestInfo,
+    @SerialName("build")
+    val build: BitriseBuildInfo?,
+)
+
+@Serializable
+data class PullRequestInfo(
     val id: Long,
     val title: String,
     val number: Int,
-    val buildTimestamp: String,
+    @SerialName("github_url")
     val githubUrl: String,
-    val buildSlug: String,
-    val artifactSlug: String,
-    val buildNumber: Int,
+    @SerialName("user_avatar_url")
     val userAvatarUrl: String,
+    @SerialName("user_login")
     val userLogin: String,
+    @SerialName("commit_sha")
     val commitSha: String,
+)
+
+@Serializable
+data class BitriseBuildInfo(
+    @SerialName("build_timestamp")
+    val buildTimestamp: String,
+    @SerialName("build_slug")
+    val buildSlug: String,
+    @SerialName("artifact_slug")
+    val artifactSlug: String,
+    @SerialName("build_number")
+    val buildNumber: Int,
 )
--- src/main/kotlin/io/github/wulkanowy/manager/server/services/UnstableService.kt
+++ src/main/kotlin/io/github/wulkanowy/manager/server/services/UnstableService.kt
@@ -1,9 +1,14 @@
 package io.github.wulkanowy.manager.server.services
 
 import io.github.wulkanowy.manager.server.models.ApiResponse
+import io.github.wulkanowy.manager.server.models.BitriseBuildInfo
 import io.github.wulkanowy.manager.server.models.PullRequestBuild
+import io.github.wulkanowy.manager.server.models.PullRequestInfo
 import io.github.wulkanowy.manager.server.repositories.BitriseRepository
 import io.github.wulkanowy.manager.server.repositories.GithubRepository
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 
 class UnstableService(
     private val githubRepository: GithubRepository,
@@ -13,25 +18,34 @@ class UnstableService(
     suspend fun getLatestBuilds(repoName: String, appId: String): ApiResponse<List<PullRequestBuild>> {
         val pullRequests = githubRepository.getPullRequests(repoName)
 
-        val builds = pullRequests.mapNotNull {
-            val build = bitriseRepository.getLastBuildsForBranch(appId, it.head.ref)
-                .getOrNull(0) ?: return@mapNotNull null
+        return coroutineScope {
+            val buildsWithPRs = pullRequests.map {
+                async { it to bitriseRepository.getLastBuildsForBranch(appId, it.head.ref) }
+            }.awaitAll()
 
-            PullRequestBuild(
-                id = it.id,
-                title = it.title,
-                number = it.number,
-                buildTimestamp = build.finishedAt,
-                githubUrl = it.htmlUrl,
-                buildSlug = build.buildSlug,
-                artifactSlug = build.artifactSlug,
-                buildNumber = build.buildNumber,
-                userAvatarUrl = it.user.avatarUrl,
-                userLogin = it.user.login,
-                commitSha = it.head.sha,
-            )
+            val builds = buildsWithPRs.map { (pr, builds) ->
+                PullRequestBuild(
+                    pullRequest = PullRequestInfo(
+                        id = pr.id,
+                        title = pr.title,
+                        number = pr.number,
+                        githubUrl = pr.htmlUrl,
+                        userAvatarUrl = pr.user.avatarUrl,
+                        userLogin = pr.user.login,
+                        commitSha = pr.head.sha,
+                    ),
+                    build = builds.firstOrNull()?.let {
+                        BitriseBuildInfo(
+                            buildTimestamp = it.finishedAt,
+                            buildSlug = it.buildSlug,
+                            artifactSlug = it.artifactSlug,
+                            buildNumber = it.buildNumber,
+                        )
+                    }
+                )
+            }
+
+            ApiResponse(success = true, data = builds)
         }
-
-        return ApiResponse(success = true, data = builds)
     }
 }
