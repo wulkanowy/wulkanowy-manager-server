b46afc1695d68721a0b83656b961e95252a48b8c
Mikołaj Pich <m.pich@outlook.com>
2021-10-30 19:37:30 +0200

    Use separate http clients for github and bitrise

--- src/main/kotlin/io/github/wulkanowy/manager/server/MainModule.kt
+++ src/main/kotlin/io/github/wulkanowy/manager/server/MainModule.kt
@@ -13,19 +13,20 @@ import io.ktor.client.features.json.serializer.*
 import io.ktor.client.features.logging.*
 import io.ktor.client.request.*
 import kotlinx.serialization.json.Json
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val mainModule = module {
     single { UnstableService(get()) }
     single { BuildsService(get()) }
-    single { GithubRepository(get()) }
-    single { BitriseRepository(get()) }
+    single { GithubRepository(get(named("github"))) }
+    single { BitriseRepository(get(named("bitrise"))) }
     single {
         Json {
             ignoreUnknownKeys = true
         }
     }
-    single {
+    single(named("bitrise")) {
         HttpClient(CIO) {
             install(Logging) {
                 level = LogLevel.INFO
@@ -35,7 +36,21 @@ val mainModule = module {
             }
             install(HttpCache)
             defaultRequest {
-                header("Authorization", "token " + System.getenv("API_KEY"))
+                header("Authorization", "token " + System.getenv("BITRISE_API_KEY"))
+            }
+        }
+    }
+    single(named("github")) {
+        HttpClient(CIO) {
+            install(Logging) {
+                level = LogLevel.INFO
+            }
+            install(JsonFeature) {
+                serializer = KotlinxSerializer(get())
+            }
+            install(HttpCache)
+            defaultRequest {
+//                header("Authorization", "token " + System.getenv("GITHUB_API_KEY"))
             }
         }
     }
